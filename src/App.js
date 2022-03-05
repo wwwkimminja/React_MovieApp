f4ee14d7fad6e1c41382a7b18aaae218ddc48539
wwwkimminja <82824976+wwwkimminja@users.noreply.github.com>
2022-03-05 22:07:47 +0900

    getMovies

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+
+import { useEffect,useState } from "react";
+import { findRenderedDOMComponentWithClass } from "react-dom/test-utils";
 
 function App() {
+  const[loading,setLoading] = useState(true);
+  const[movies,setMovies]=useState([]);
+  const getMovies = async()=>{
+    const response = await fetch(`https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year`)
+    const json = await response.json();
+    setMovies(json.data.movies);
+    setLoading(false);
+  }
+  useEffect(()=>{
+    getMovies();
+  },[]);
+console.log(movies);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {loading ? (<h1>Loading...</h1>) : 
+      <div>
+        {movies.map((movie)=>(
+          <div key={movie.id}>
+            <img src={movie.medium_cover_image}/>
+            <h2>{movie.title}</h2>
+            <p>{movie.summary}</p>
+            <ul>
+              {movie.genres.map((g)=><li key={g}>{g}</li>)}
+            </ul>
+            </div>
+        ))}
+
+        </div>
+      }
     </div>
   );
 }
