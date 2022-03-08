240f23018d517a495a19d41cbbe8b6b47f7fe925
wwwkimminja <82824976+wwwkimminja@users.noreply.github.com>
2022-03-08 20:29:29 +0900

    Router basename

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ import {
 
 function App() {
   return (
-  <Router>
+  <Router basename={process.env.PUBLIC_URL}>
     <Routes>
       <Route path ="/movie/:id" element ={<Detail />}>
       </Route>
